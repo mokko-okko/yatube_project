cdc19275fbea6fb95148603dccc728dec87710ff
Юрий Костров <kostrov.yk@yandex.ru>
2022-04-09 21:11:22 +0300

    с нуля

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#68rtf413$q9$q_k!_j%9&wq)znifc*n9*f3p=d^j13$#andwo'
+SECRET_KEY = 'kd_sm3ahqbl%g*m1^7kk39^*&(2wdj1*#5beg35y)utxm1#mw7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
